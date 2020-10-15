2aeaa8d018797efe600dc6a6950b425522da08a6
Ionut Badea <ionut.badea@yazee.me>
2020-10-15 13:17:13 +0300

    changed version in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name='envtypes',
-    version='0.3.0',
+    version='0.3.1',
     author='YazeeMe',
     author_email='ionut.badea@yazee.me',
     description="Return the values of the envs with the correct type.",
